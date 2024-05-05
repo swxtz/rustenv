bd400d1c3b73b0a773003dd1a8aaa4b005360e14
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-05-05 20:16:40 -0300

    feat: create write feature

---
+++ src/write.rs
@@ -0,0 +1,21 @@
+use std::env::set_var;
+use std::error::Error;
+use std::fmt::format;
+
+pub struct Write;
+
+impl Write {
+    pub fn new(key: String, value: String) -> Result<(), String> {
+        let env = set_var(key, value);
+
+        match env {
+            Some => {
+                let result = format!("Error: env {} write", value);
+                return Err(result);
+            },
+            None() => {
+                Ok(())
+            }
+        }
+    }
+}
